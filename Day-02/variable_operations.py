5ea587a1a7df76d45ade400e63a8e06ae11e4148
gudipatinikhil93 <gudipatinikhil93@gmail.com>
2026-08-05 17:47:01 +0530

    Python Intro

---
+++ Day-02/variable_operations.py
@@ -0,0 +1,31 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: D:/PythonCourseWork/Day-02/keywords.py ===============
+['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+36
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c = 12,13,14
+>>> a
+12
+>>> b
+13
+>>> c
+14
+>>> var=12
+>>> var
+12
+>>> 1var=12
+SyntaxError: invalid syntax
+>>> var 12 = 13
+SyntaxError: invalid syntax
+>>> vae13=13
+>>> vae13
+13
+>>> 
